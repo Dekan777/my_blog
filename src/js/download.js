c33c03e7ac9f2c78341bfa68d9268a0f6d9e2565
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2025-01-09 10:48:07 +0100

    start GHP

---
+++ src/js/download.js
@@ -0,0 +1,9 @@
+export function downloadFile() {
+  const fileUrl = '../public/Denys_Koshevyi_FrontEndDeveloper.pdf'; // Путь к файлу
+  const a = document.createElement('a');
+  a.href = fileUrl;
+  a.download = 'Denys_Koshevyi_FrontEndDeveloper.pdf'; // Имя файла
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
